8407c794af74076873ecbd4458a12b95fb2ba2b0
Sabri Nu <sabrinuzi@gmail.com>
2020-04-22 18:57:21 +0200

    Move UI to base repo

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # megroplan-firefox
+Simple Firefox extension to easily save any web recipe on Megroplan.
+
+This uses project [https://github.com/snuzi/megroplan-browser-base](https://github.com/snuzi/megroplan-browser-base)
+as submodule.
+
+### Clone project
+`git clone --recursive this-repo-url`
+
+or after **git clone** only
+`git submodule update --init --recursive`
--- popup.js
+++ popup.js
@@ -1,14 +1,15 @@
-var megroplanUrl = "https://megroplan.com/app/recipe/scrape?url=";
+var megroplan = new Megroplan();
 
 document.addEventListener("click", function(e) {
 	if (e.target.id == 'saveRecipe') {
 		browser.tabs.query({active: true, windowId: browser.windows.WINDOW_ID_CURRENT})
 			.then(tabs => browser.tabs.get(tabs[0].id))
 			.then(tab => {
-				var chosenPage = megroplanUrl + tab.url;
-				browser.tabs.create({
-					url: chosenPage
-				});
+                megroplan.scrapeWebsite(tab.url, function(_url) {
+                    browser.tabs.create({
+                        url: _url
+                    });
+                });
 			});		
 	}
 });
